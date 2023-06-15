a1fbad979688c28564e1cf57e06e14ec65ac96f0
Karasp1980 <karasp80@gmail.com>
2023-06-15 21:35:14 +0000

    add logo

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -28,7 +28,8 @@ function PostsPage({ message, filter = "" }) {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+        
+        const { data } = await axiosReq.get(`/posts/?search=${query}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
